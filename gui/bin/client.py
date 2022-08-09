516d50fefa7262abac8419cf16049d0a5e03a321
MbGen <redpyl6@gmail.com>
2022-08-09 16:11:03 +0300

    recive json data about player info

--- gui/bin/client.py
+++ gui/bin/client.py
@@ -3,15 +3,15 @@ import json
 from pprint import pprint
 import eel
 import time
+from .main import Player
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 BUFF = 1024
 DISCONNECT_MSG = "!DISCONNECT"
-is_connected = False
 
 
 def connect(ip, port):
-    global is_connected
     try:
         client.connect((ip, port))
         eel.writeAlert(f"You are connected to {ip}")
@@ -19,14 +19,8 @@ def connect(ip, port):
         while True:
             data = client.recv(BUFF)
             player = json.loads(data.decode("utf-8"))
-            pprint(player)
-            time.sleep(2)
-            # if input().lower() == "q":
-            #     send(DISCONNECT_MSG)
-            #     logger.info("Disconnecting")
-            #     client.close()
-            #     sys.exit()
-
+            player = Player(**player) 
+            eel.showPlayer(player)
     except ConnectionRefusedError:
         eel.writeAlert("Unable to connect")
         return
@@ -38,8 +32,3 @@ def connect(ip, port):
 def send(msg: str):
     message = msg.encode("utf-8")
     client.send(message)
-
-
-def connected():
-    global is_connected
-    return is_connected
